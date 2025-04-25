7e66833b65b682dd6d0bac400e1b018e6297e25f
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-04-25 14:02:03 +0530

    Implemented UC6_Logging

--- src/main/java/com/example/address_book/Model/Address.java
+++ src/main/java/com/example/address_book/Model/Address.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Address {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -16,36 +15,45 @@ public class Address {
     private String phoneNumber;
     private String email;
     private String city;
+
     public Long getId() {
-        return id;
+        return id ;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getFullName() {
         return fullName;
     }
+
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
-    
 }
 
