8bda4fb741dc2e04c7b9eb245aeebaa60cd97ae8
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-04-25 14:50:45 +0530

    Implemented UC6

--- src/main/java/com/example/address_book/Service/AddressService.java
+++ src/main/java/com/example/address_book/Service/AddressService.java
@@ -3,6 +3,7 @@ package com.example.address_book.Service;
 import java.util.*;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,48 +13,77 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.example.address_book.Model.Address;
 import com.example.address_book.Repo.AddressRepo;
 
+@Slf4j
 @Service
 public class AddressService {
   @Autowired
-  public AddressRepo addressRepo ; 
+  public AddressRepo addressRepo ;
 
   public ResponseEntity<Address> addAddress(@RequestBody Address address){
-    addressRepo.save(address); 
-    return new ResponseEntity<>(address,HttpStatus.OK) ; 
+    try {
+      Address savedAddress = addressRepo.save(address);
+      log.debug("Address saved successfully: {}", savedAddress.toString());
+      return new ResponseEntity<>(savedAddress, HttpStatus.OK);
+    } catch (Exception e) {
+      log.error("Error occurred while saving address: {}", address, e);
+      throw new RuntimeException(e);
+    }
   }
 
   public List<Address> getAllAddresses() {
-    return addressRepo.findAll();
+    log.info("Get HTTP Method called");
+    try{
+      log.debug("the list will be returned here.");
+      return addressRepo.findAll();}
+    catch (Exception e) {
+      log.error("Error occurred ,exception handled");
+      throw new RuntimeException(e);
+    }
   }
 
-  public ResponseEntity<Address> getAddressById(Long id){
-    Optional<Address> address = addressRepo.findById(id) ; 
+  public ResponseEntity<Address> getAddressById(Long id) {
+    log.info("GET /addresses/{} - Fetching address by ID", id);
+    Optional<Address> address = addressRepo.findById(id);
 
-    if(address.isPresent()){
-      return new ResponseEntity<>(address.get() , HttpStatus.OK) ; 
-    }return new ResponseEntity<>(HttpStatus.NOT_FOUND) ; 
+    if (address.isPresent()) {
+      log.debug("Address found with ID {}", id);
+      return new ResponseEntity<>(address.get(), HttpStatus.OK);
+    } else {
+      log.warn("No address found with ID {}", id);
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
   }
 
   public ResponseEntity<Address> updateAddress(Long id, Address updated_address){
-    Optional<Address> address = addressRepo.findById(id); 
-    
+    log.info("Put /addresses/{} - Updating address by id",id);
+    Optional<Address> address = addressRepo.findById(id);
+
     if(address.isPresent()){
+
       address.get().setCity(updated_address.getCity());
       address.get().setEmail(updated_address.getEmail());
       address.get().setFullName(updated_address.getFullName());
       address.get().setPhoneNumber(updated_address.getPhoneNumber());
       address.get().setId(updated_address.getId());
 
-      return new ResponseEntity<>(address.get(),HttpStatus.OK) ; 
-    }return new ResponseEntity<>(HttpStatus.NOT_FOUND) ; 
+      log.debug("Address updated with ID {}", id);
+
+      return new ResponseEntity<>(address.get(),HttpStatus.OK) ;
+    }
+    log.warn("Attempted to update non-existing address with ID {}", id) ;
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND) ;
   }
-  
-  public ResponseEntity<Address> deleteAddress(Long id) { 
-    List<Address> data = addressRepo.findAll() ; 
-    Address address = addressRepo.findById(id).get() ; 
-    if(data.contains(address)){
-      addressRepo.deleteById(id); 
-      return new ResponseEntity<>(HttpStatus.OK) ; 
-    }return new ResponseEntity<>(HttpStatus.NOT_FOUND); 
-   }
+
+  public ResponseEntity<Address> deleteAddress(Long id) {
+    log.info("Delete /addresses/{} - Deleting address by id",id);
+    Optional<Address> address = addressRepo.findById(id) ;
+    if(address.isPresent()){
+      addressRepo.deleteById(id);
+
+      log.debug("Address deleted with ID {}", id);
+      return new ResponseEntity<>(HttpStatus.OK) ;
+    }
+    log.warn("Attempted to delete non-existing address with ID {}", id) ;
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
 }
